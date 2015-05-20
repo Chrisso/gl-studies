42677383f0340b707fd16287446f3bb460e6a60b
Christoph <christoph@stoepel.net>
2015-05-20 14:59:40 +0200

    Create window with own device context.

--- TemplateWinApi/TemplateWinApi.cpp
+++ TemplateWinApi/TemplateWinApi.cpp
@@ -66,7 +66,7 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	
 	WNDCLASSEX wcex;
 	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
 	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
