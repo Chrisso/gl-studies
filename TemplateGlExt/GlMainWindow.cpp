6c06d5748d369dd0f37b5b37cfd81c8ff170e99d
Christoph D. Stoepel <christoph@stoepel.net>
2018-05-20 10:13:24 +0200

    Separate OpenGL child view

--- TemplateGlExt/GlMainWindow.cpp
+++ TemplateGlExt/GlMainWindow.cpp
@@ -36,71 +36,17 @@ int CGlMainWindow::OnCreate(CREATESTRUCT *lpcs)
 	m_StatusBar.SetPanes(nPanes, sizeof(nPanes) / sizeof(int), false);
 	m_StatusBar.SetPaneText(ID_DEFAULT_PANE, _T("Ready"));
 
-	m_hDC = GetDC();
-	if (!m_hDC)
-	{
-		::AtlMessageBox(m_hWnd, IDS_ERR_OPENGL, IDR_MAINFRAME);
-		return -1;
-	}
-
 	if (!InitGlew())
 	{
 		::AtlMessageBox(m_hWnd, IDS_ERR_OPENGL, IDR_MAINFRAME);
 		return -1;
 	}
 
-	UINT nFormats = 0;
-	int nPixelFormat = 0;
-	int nAttributes[] = {
-		WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
-		WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
-		WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
-		WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
-		WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
-		WGL_COLOR_BITS_ARB, ::GetDeviceCaps(m_hDC, BITSPIXEL),
-		WGL_DEPTH_BITS_ARB, 16,
-		WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
-		WGL_SAMPLES_ARB, 8,
-		0
-	};
-
-	if (!wglChoosePixelFormatARB(m_hDC, nAttributes, NULL, 1, &nPixelFormat, &nFormats) || !nFormats)
-	{
-		::AtlMessageBox(m_hWnd, IDS_ERR_OPENGL, IDR_MAINFRAME);
-		return -1;
-	}
-
-	PIXELFORMATDESCRIPTOR pfd;
-
-	if (!nPixelFormat ||
-		!::DescribePixelFormat(m_hDC, nPixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd) ||
-		!::SetPixelFormat(m_hDC, nPixelFormat, &pfd))
-	{
-		::AtlMessageBox(m_hWnd, IDS_ERR_OPENGL, IDR_MAINFRAME);
-		return -1;
-	}
-
-	int nContextAttributes[] = {
-		WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-		WGL_CONTEXT_MINOR_VERSION_ARB, 0,
-		WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
-		WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
-		0
-	};
-
-	m_hRC = wglCreateContextAttribsARB(m_hDC, NULL, nContextAttributes);
-	if (!m_hRC || !wglMakeCurrent(m_hDC, m_hRC))
-	{
-		::AtlMessageBox(m_hWnd, IDS_ERR_OPENGL, IDR_MAINFRAME);
-		return -1;
-	}
-
-	m_pScene = new CScene();
-	if (!m_pScene->Create())
-	{
-		::AtlMessageBox(m_hWnd, IDS_ERR_OPENGL, IDR_MAINFRAME);
-		return -1;
-	}
+	m_hWndClient = m_View.Create(
+		m_hWnd, rcDefault, NULL,
+		WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+		WS_EX_CLIENTEDGE
+	);
 
 	m_FrameCounter.SetTargetWindow(m_hWnd);
 
@@ -127,39 +73,7 @@ int CGlMainWindow::OnClose()
 int CGlMainWindow::OnDestroy()
 {
 	ATLTRACE(_T("OnDestroy\n"));
-
-	if (m_pScene)
-	{
-		delete m_pScene;
-		m_pScene = NULL;
-	}
-
-	if (m_hRC)
-	{
-		wglMakeCurrent(NULL, NULL);
-		wglDeleteContext(m_hRC);
-		m_hRC = NULL;
-	}
-
-	if (m_hDC)
-	{
-		ReleaseDC(m_hDC);
-		m_hDC = NULL;
-	}
-
 	PostQuitMessage(0);
-	return 0;
-}
-
-int CGlMainWindow::OnSize(UINT nType, CSize size)
-{
-	if (m_pScene)
-	{
-		glViewport(0, 0, size.cx, size.cy);
-		m_pScene->Resize(size.cx, size.cy);
-	}
-
-	SetMsgHandled(FALSE);
 	return 0;
 }
 
@@ -221,7 +135,7 @@ bool CGlMainWindow::InitGlew()
 			pfd.nVersion = 1;
 			pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
 			pfd.iPixelType = PFD_TYPE_RGBA;
-			pfd.cColorBits = ::GetDeviceCaps(m_hDC, BITSPIXEL);
+			pfd.cColorBits = ::GetDeviceCaps(hDC, BITSPIXEL);
 
 			int iPixelFormat = ::ChoosePixelFormat(hDC, &pfd);
 			if (iPixelFormat && ::SetPixelFormat(hDC, iPixelFormat, &pfd))
@@ -254,12 +168,5 @@ bool CGlMainWindow::InitGlew()
 
 void CGlMainWindow::Render()
 {
-	if (!m_hDC || !m_hRC) return;
-
-	if (m_pScene)
-	{
-		m_pScene->Render(m_FrameCounter.Tick() / 10.0f);
-	}
-
-	::SwapBuffers(m_hDC);
+	m_View.Render(m_FrameCounter.Tick() / 10.0f);
 }
--- TemplateGlExt/GlMainWindow.h
+++ TemplateGlExt/GlMainWindow.h
@@ -5,18 +5,15 @@
 #include <FrameCounter.h>
 
 #include "Resource.h"
-#include "Scene.h"
+#include "GlView.h"
 
 class CGlMainWindow : public CFrameWindowImpl<CGlMainWindow>
 {
 private:
-	HDC   m_hDC = NULL;
-	HGLRC m_hRC = NULL;
-
-	CScene *m_pScene = nullptr;
 	CFrameCounter m_FrameCounter;
 	CCommandBarCtrl m_CmdBar;
 	CMultiPaneStatusBarCtrl m_StatusBar;
+	CGlView m_View;
 
 public:
 	CGlMainWindow();
@@ -28,20 +25,26 @@ public:
 	int OnCreate(CREATESTRUCT *lpcs);
 	int OnClose();
 	int OnDestroy();
-	int OnSize(UINT nType, CSize size);
 
 	void OnFileExit(UINT uNotifyCode, int nID, HWND hWnd);
 	void OnHelpInfo(UINT uNotifyCode, int nID, HWND hWnd);
 
 	LRESULT OnBenchmark(UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-	DECLARE_FRAME_WND_CLASS_EX(_T("CS_TEMPLATEGL"), IDR_MAINFRAME, CS_OWNDC | CS_DBLCLKS, COLOR_WINDOW + 1);
+	virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		if (CFrameWindowImpl<CGlMainWindow>::PreTranslateMessage(pMsg))
+			return TRUE;
+
+		return m_View.PreTranslateMessage(pMsg);
+	}
+
+	DECLARE_FRAME_WND_CLASS_EX(_T("CS_TEMPLATEGL"), IDR_MAINFRAME, 0, COLOR_WINDOW + 1);
 
 	BEGIN_MSG_MAP_EX(CGlMainWindow)
 		MSG_WM_CREATE(OnCreate)
 		MSG_WM_CLOSE(OnClose)
 		MSG_WM_DESTROY(OnDestroy)
-		MSG_WM_SIZE(OnSize)
 
 		COMMAND_ID_HANDLER_EX(IDM_EXIT, OnFileExit)
 		COMMAND_ID_HANDLER_EX(IDM_ABOUT, OnHelpInfo)
