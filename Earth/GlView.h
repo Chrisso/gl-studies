a3747e872226210d3b8f4c99d8cd8e51199fe2fb
Christoph D. Stoepel <942416+Chrisso@users.noreply.github.com>
2018-06-23 10:27:46 +0200

    Init Earth app

---
+++ Earth/GlView.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class CGlView : public CWindowImpl<CGlView>
+{
+private:
+	HDC     m_hDC = NULL;
+	HGLRC   m_hRC = NULL;
+
+public:
+	CGlView();
+	~CGlView();
+
+	void Render(float time);
+
+	int OnCreate(CREATESTRUCT *lpcs);
+	int OnDestroy();
+	int OnSize(UINT nType, CSize size);
+
+	void OnDebugMessage(
+		GLenum source, GLenum type, GLuint id,
+		GLenum severity, LPCTSTR szMsg) const;
+
+	BOOL PreTranslateMessage(MSG*)
+	{
+		return FALSE;
+	}
+
+	DECLARE_WND_CLASS_EX(NULL, CS_OWNDC, 0)
+
+	BEGIN_MSG_MAP_EX(CGlView)
+		MSG_WM_CREATE(OnCreate)
+		MSG_WM_DESTROY(OnDestroy)
+		MSG_WM_SIZE(OnSize)
+	END_MSG_MAP()
+};
---
+++ Earth/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Earth.rc
+//
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SMALL                       106
+#define IDR_MAINFRAME                   128
+#define ID_INFO_PANE                    129
+#define IDS_APP_INFO                    130
+#define IDS_ERR_OPENGL                  131
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           107
+#endif
+#endif
